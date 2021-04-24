447f9df7e076dbce58c40fc00fa43491f7646ba1
Nabil Fawzy <Nabilfawzy507@gmail.com>
2021-04-24 04:42:25 +0200

    .

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=01141942205/devopsproject4
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag devopsproject4  $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
