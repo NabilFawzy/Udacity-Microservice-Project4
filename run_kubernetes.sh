375c0308fffd84895c27a46a34b1207ab7b0c8ff
Nabil Fawzy <Nabilfawzy507@gmail.com>
2021-04-24 03:31:27 +0200

    .

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath=<>  dockerpath="noahgift/flasksklearn"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+#kubectl run flaskskearlndemo\
+    #--generator=run-pod/v1\
+    #--image=$dockerpath\
+    #--port=80 --labels app=flaskskearlndem
 
 # Step 3:
-# List kubernetes pods
+# List kubernetes pods  -kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
+# Forward the container port to a host -kubectl port-forward flaskskearlndemo 8000:80
 
