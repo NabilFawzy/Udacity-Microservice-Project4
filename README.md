c916f4005a4184b0937432d713b91ee8bbc05fcf
Nabil Fawzy <Nabilfawzy507@gmail.com>
2021-04-24 14:00:39 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Udacity-Microservice-Project4
+
+
+[![CircleCI](https://circleci.com/gh/circleci/circleci-docs.svg?style=svg)](https://circleci.com/gh/circleci/Udacity-Microservice-Project4)
+
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,19 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>  dockerpath="noahgift/flasksklearn"
+dockerpath=01141942205/devopsproject4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-#kubectl run flaskskearlndemo\
-    #--generator=run-pod/v1\
-    #--image=$dockerpath\
-    #--port=80 --labels app=flaskskearlndem
+kubectl run devopsproject4\
+    --image=$dockerpath\
+    --port=80 
 
 # Step 3:
-# List kubernetes pods  -kubectl get pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host -kubectl port-forward flaskskearlndemo 8000:80
+# Forward the container port to a host 
+kubectl port-forward devopsproject4 8000:80
 
